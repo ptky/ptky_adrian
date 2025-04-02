449c7b9fe00cab82e61ecbe8207dbbb2afa6d105
adrianhelllo <hetessyadrian@gmail.com>
2025-04-02 19:56:38 +0200

    sssssasasaa

--- metodusok2.py
+++ metodusok2.py
@@ -1,22 +1,24 @@
 import random
 
-nyugta_szel = 15
+nyugta_szel = 30
 
-def elvalaszto(hossz):
-    return hossz * "*"
+def elvalaszto(hossz, char):
+    return hossz * char
 
 
 def lec(szel, szov):
-    print(elvalaszto(szel))
-    print(f"*{szov:^{szel - 2}}*")
-    print(elvalaszto(szel))
+    print(elvalaszto(szel * 2, "*"))
+    print(f"*{szov:^{(szel - 1) * 2}}*")
+    print(elvalaszto(szel * 2, "*"))
 
 def tetelek(tetelek, szel):
     for tetel in tetelek:
         ar = random.randint(200, 6000)
         ar_str = str(ar) + ' Ft'
         print(f"{tetel:<{szel}}{ar_str:>{szel}}")
+    print(elvalaszto(szel * 2, "="))
 
 tetelek_j = [f'tétel{i + 1}' for i in range(10)]
 
+lec(nyugta_szel, "NYUGTA")
 tetelek(tetelek_j, nyugta_szel)
